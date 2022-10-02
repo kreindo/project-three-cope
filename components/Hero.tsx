bbec1c9318fd4157dd21e6482b2875d5b120757e
kreindo <ahmadsan4p@gmail.com>
2022-10-02 09:31:59 +0800

    test

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,4 @@
-/* This example requires Tailwind CSS v2.0+ */ import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
+/* This example requires Tailwind CSS v2.0+ */ import { Fragment } from 'react';import { Popover, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 
@@ -66,7 +65,7 @@ export default function Hero(): JSX.Element {
                   ))}
                   <a
                     href="#"
-                    className="font-medium text-primary hover:text-primary"
+                    className="font-medium text-secondary hover:text-secondary"
                   >
                     Maps
                   </a>
@@ -130,10 +129,8 @@ export default function Hero(): JSX.Element {
           <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">
-                  Masjid Jami&apos; Nurul Iman
-                </span>{' '}
-                <span className="block text-primary xl:inline">
+                <span className="block xl:inline">Masjid Raydhatul Jannah</span>{' '}
+                <span className="block text-secondary xl:inline">
                   Landing Page
                 </span>
               </h1>
