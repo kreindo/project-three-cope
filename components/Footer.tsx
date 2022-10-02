3f06443925d6e0dba214eb368c5022aeb1b09904
kreindo <ahmadsan4p@gmail.com>
2022-10-02 08:52:35 +0800

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
-import {
-  FaFacebook,
+import {  FaFacebook,
   FaWhatsapp,
   FaInstagram,
   FaTwitter,
@@ -9,9 +8,9 @@ import Image from 'next/image';
 const socialData = [];
 export default function Footer() {
   return (
-    <footer className="h-32 flex items-center w-full absolute bottom-0 bg-white">
-      <div className="flex w-full max-w-7xl mx-auto justify-between items-center">
-        <div className="flex flex-col items-start">
+    <footer className="h-32 flex items-center px-10 border-2 border-red-500 mx-auto w-full absolute bottom-0 bg-white">
+      <div className="flex flex-col sm:flex-row gap-0 sm:gap-10 w-full max-w-screen-xl mx-auto justify-between items-center">
+        <div className="flex flex-col items-center sm:items-start">
           <div>
             <Image src="/logo/logoFn.svg" alt="logo" width={24} height={24} />
             <h1 className="text-gray-500 text-1xl">
